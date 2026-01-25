50b4b4bea6a0c65371edb621c233c6536a4359db
José Henrique Coelho <coelhojh@gmail.com>
2026-01-25 12:19:48 -0300

    test: production deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-purple-600 text-white text-3xl font-bold">
-      Mantivo Preview funcionando 🚀
+      Mantivo Produção funcionando 🚀
     </div>
   );
 }
